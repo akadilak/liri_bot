35e02c88fa102fae4e4353331925674de9b32559
unknown <amkadilak@gmail.com>
2018-01-07 20:05:19 -0500

    finally got it up and running. I hate semicolons

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log("Keys loaded");
+
+var twitterKeys = {
+  consumer_key: "hKD4m1ZQw5teaSaQNv0wcf7DK",
+  consumer_secret: "8VsXo42ScYeInxC0KyQ2j8YFpowLSVP6SiB4cDlKjDjYHGosTc",
+  access_token_key: "950158399735463936-WgOZ0Z5PFRtvXD3c0njChWYuAEP6HWo",
+  access_token_secret: "ijQLLV8pgnztzJUzQ1NoV3zBYlcb76euZHfS32WSgglEg"
+};
+module.exports = twitterKeys;
